783a2ce46fe41e12d93fd15776724fb818e52647
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-19 14:28:21 -0700

    Ignore fps option if fps=-1

--- backend/extract_frames.py
+++ backend/extract_frames.py
@@ -2,7 +2,7 @@ from ffmpeg import FFmpeg
 
 # download_mp4("https://jasonfeng365.github.io/canis/canis-contests.mp4", 'videos/out.mp4')
 # extract_frames("videos/", "out.mp4")
-def extract_frames(path, videoName, fps=1):
+def extract_frames(path, videoName, fps=-1):
 	if path[-1] != '/': path += '/'
 	videoPath = path + videoName
 	framesPath = path + "frame_%05d.jpg"
@@ -14,8 +14,10 @@ def extract_frames(path, videoName, fps=1):
 		FFmpeg()
 		.option("i", videoPath)
 		.option('y')
-		.option('vf', f'fps={fps}')
 		.output(framesPath)
 	)
+
+	
+	if fps>0: ffmpeg.option('vf', f'fps={fps}')
 
 	ffmpeg.execute()
