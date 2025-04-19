d10bec9e911966888e4544f9762592cf614e9fc5
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-19 15:35:03 -0700

    Prevent upscaling, begin process_video

--- backend/mp4dl.py
+++ backend/mp4dl.py
@@ -2,11 +2,11 @@ from ffmpeg import FFmpeg
 
 
 def scale_video(url, outputFile, area=10000):
-    # -vf scale=2*floor(sqrt(1000*iw/ih)/2):2*floor(1000/sqrt(1000*iw/ih)/2),setsar=1:1
-    widthEq = f"2*floor(sqrt({area}*iw/ih)/2)"
-    heightEq = f"2*floor({area}/sqrt({area}*iw/ih)/2)"
-    vf = f"scale={widthEq}:{heightEq},setsar=1:1"
-    print(vf)
-    ffmpeg = FFmpeg().option("i", url).option("vf", vf).option("y").output(outputFile)
+	# -vf scale=2*floor(sqrt(1000*iw/ih)/2):2*floor(1000/sqrt(1000*iw/ih)/2),setsar=1:1
+	widthEq = f"'2*floor(min(iw,sqrt({area}*iw/ih))/2)'"
+	heightEq = f"'2*floor(min(ih,{area}/sqrt({area}*iw/ih))/2)'"
+	vf = f"scale={widthEq}:{heightEq},setsar=1:1"
+	print(vf)
+	ffmpeg = FFmpeg().option("i", url).option("vf", vf).option("y").output(outputFile)
 
-    ffmpeg.execute()
+	ffmpeg.execute()
