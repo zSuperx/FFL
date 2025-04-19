82eade63952aa146e04581dbaacee173d04bfe91
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-19 13:18:16 -0700

    Add auto scaling to mp4dl.py

--- backend/mp4dl.py
+++ backend/mp4dl.py
@@ -1,9 +1,15 @@
 from ffmpeg import FFmpeg
 
-def download_mp4(url, outputFile):
+def download_mp4(url, outputFile, area=10000):
+	# -vf "scale=2*floor(sqrt(1000*iw/ih)/2):2*floor(1000/sqrt(1000*iw/ih)/2),setsar=1:1"
+	widthEq = f'2*floor(sqrt({area}*iw/ih)/2)'
+	heightEq = f'2*floor({area}/sqrt({area}*iw/ih)/2)'
+	vf = f'scale={widthEq}:{heightEq},setsar=1:1'
+	print(vf)
 	ffmpeg = (
 		FFmpeg()
 		.option("i", url)
+		.option("vf", vf)
 		.option('y')
 		.output(
 			outputFile
@@ -14,6 +20,9 @@ def download_mp4(url, outputFile):
 			# crf=24,
 		)
 	)
+
+	print(" ".join(ffmpeg.arguments))
+	# print(ffmpeg.)
 
 	ffmpeg.execute()
 
