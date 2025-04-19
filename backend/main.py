827410ef3adcc2313ed333bade04bb876f11d8d6
zsuper <piyushkumbhare10@gmail.com>
2025-04-19 15:25:57 -0700

    updated file inputs

--- backend/main.py
+++ backend/main.py
@@ -2,18 +2,22 @@ import os
 from ytdl import download_yt_vid
 from mp4dl import download_mp4
 
-# URL = "https://www.youtube.com/watch?v=6yrdS4tIP9U"
-URL = "https://www.youtube.com/watch?v=uHU4_Wo4_OY"
+URL = "https://www.youtube.com/watch?v=6yrdS4tIP9U"
+# URL = "https://www.youtube.com/watch?v=uHU4_Wo4_OY"
 
 
 def main():
     print("Hello from ffl!")
 
-    video_path = os.path.join("videos", "test-video1")
+    pathname = os.path.join("videos", "test-video1")
 
-    download_yt_vid(URL, video_path)
+    video_path = download_yt_vid(URL, pathname)
 
-    download_mp4(f"{video_path}.webm", f"{video_path}-scaled.webm", 8)
+    print(f"{video_path = }")
+    video_path = os.path.relpath(video_path)
+
+    print(f"{video_path = }")
+    download_mp4(video_path, f"{pathname}-scaled.webm")
 
 
 if __name__ == "__main__":
--- backend/ytdl.py
+++ backend/ytdl.py
@@ -2,8 +2,13 @@ import os
 import subprocess
 
 
-def download_yt_vid(url: str, output_path: str):
-    result = subprocess.run(["yt-dlp", url, "-o", output_path])
+def download_yt_vid(url: str, output_path: str) -> str:
+    result = subprocess.run(
+        ["yt-dlp", "--print", "after_move:filepath", "-o", output_path, url],
+        capture_output=True,
+    )
     if result.returncode != 0:
         print("Error occurred while running `yt-dlp`")
         exit(1)
+    else:
+        return result.stdout.strip().decode()
