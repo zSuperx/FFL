f0d6bc85a92b14136515fab10984221304d24085
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-19 13:20:16 -0700

    Clean up mp4dl.py comments

--- backend/mp4dl.py
+++ backend/mp4dl.py
@@ -1,7 +1,7 @@
 from ffmpeg import FFmpeg
 
 def download_mp4(url, outputFile, area=10000):
-	# -vf "scale=2*floor(sqrt(1000*iw/ih)/2):2*floor(1000/sqrt(1000*iw/ih)/2),setsar=1:1"
+	# -vf scale=2*floor(sqrt(1000*iw/ih)/2):2*floor(1000/sqrt(1000*iw/ih)/2),setsar=1:1
 	widthEq = f'2*floor(sqrt({area}*iw/ih)/2)'
 	heightEq = f'2*floor({area}/sqrt({area}*iw/ih)/2)'
 	vf = f'scale={widthEq}:{heightEq},setsar=1:1'
@@ -11,18 +11,8 @@ def download_mp4(url, outputFile, area=10000):
 		.option("i", url)
 		.option("vf", vf)
 		.option('y')
-		.output(
-			outputFile
-			# "output.mp4",
-			# {"codec:v": "libx264"},
-			# vf="scale=1280:-1",
-			# preset="veryslow",
-			# crf=24,
-		)
+		.output(outputFile)
 	)
-
-	print(" ".join(ffmpeg.arguments))
-	# print(ffmpeg.)
 
 	ffmpeg.execute()
 
