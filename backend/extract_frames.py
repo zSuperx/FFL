a6cda3fd458481f95a1bb16a4de4cb4762cd7d6d
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-04-19 14:11:30 -0700

    Create extract_frames.py

---
+++ backend/extract_frames.py
@@ -0,0 +1,20 @@
+from ffmpeg import FFmpeg
+
+# download_mp4("https://jasonfeng365.github.io/canis/canis-contests.mp4", 'videos/out.mp4')
+# extract_frames("videos/", "out.mp4")
+def extract_frames(path, videoName):
+	if path[-1] != '/': path += '/'
+	videoPath = path + videoName
+	framesPath = path + "frame_%05d.jpg"
+
+	print(videoPath)
+	print(framesPath)
+
+	ffmpeg = (
+		FFmpeg()
+		.option("i", videoPath)
+		.option('y')
+		.output(framesPath)
+	)
+
+	ffmpeg.execute()
