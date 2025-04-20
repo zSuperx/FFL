596731ca4fad354bc0b1e9e06d5b347d04e08c72
audrey <audrey@gobaco.com>
2025-04-19 23:03:23 -0700

    god i hope this works

--- backend/extract_frames.py
+++ backend/extract_frames.py
@@ -5,10 +5,9 @@ import numpy as np
 
 # download_mp4("https://jasonfeng365.github.io/canis/canis-contests.mp4", 'videos/out.mp4')
 # extract_frames("videos/", "out.mp4")
-def extract_frames(path, videoName, fps=-1):
-	if path[-1] != '/': path += '/'
-	videoPath = path + videoName
-	framesPath = path + "frame_%05d.jpg"
+def extract_frames(dir: str, video_file: str, fps: int):
+	videoPath = os.path.join(dir, video_file)
+	framesPath = os.path.join(dir, "frame_%05d.jpg")
 
 	print(videoPath)
 	print(framesPath)
@@ -19,16 +18,16 @@ def extract_frames(path, videoName, fps=-1):
 		.option('y')
 		.output(framesPath)
 	)
-
-	
 	if fps>0: ffmpeg.option('vf', f'fps={fps}')
-
 	ffmpeg.execute()
 
 # Return SUSSY frames of diff
-def compare_frames(path):
+def compare_frames(dir: str, fps: int, chunk_size=100):
+	# How long do we wait until we stop trying to merge flashes?
+	patience = 10*fps
+
 	frameIdx = 1
-	framePath = os.path.join(path, "frame_%05d.jpg" % frameIdx)
+	framePath = os.path.join(dir, "frame_%05d.jpg" % frameIdx)
 	# There must be at least one frame
 	if not os.path.exists(framePath):
 		return None
@@ -43,10 +42,14 @@ def compare_frames(path):
 			]
 	frameIdx += 1
 
-	# List of diffs from each of the image formats
+	# Yield intervals (start, delta) of flashing lights
 	out = []
+	start = None
+	end = None
+	buffer = []
+	countdown = patience
 	while os.path.exists(framePath):
-		print(f"Working on ({frameIdx-1}, {frameIdx})")
+		# print(f"Working on ({frameIdx-1}, {frameIdx})")
 		frame = cv2.imread(framePath)
 		# frame = frame.astype(np.int16)
 		new_prev = [
@@ -54,41 +57,80 @@ def compare_frames(path):
 			convolve_frame(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)),
 		]
 		# If either RGB or HSV say there's a huge change, flag it.
-		if diff_frames(prev[0], new_prev[0]) or diff_frames(prev[1], new_prev[1]):
-			out.append((frameIdx-1, frameIdx))
+		if diff_bright(prev[1], new_prev[1]) and diff_avg(prev[0], new_prev[0]):
+			# Start interval
+			if start == None:
+				start = frameIdx-1
+				end = frameIdx
+			else:
+				end = frameIdx
+				countdown = patience
 		
 		# Get next frame
 		prev = new_prev
 		frameIdx += 1
-		framePath = os.path.join(path, "frame_%05d.jpg" % frameIdx)
-		print()
-	return out
+		framePath = os.path.join(dir, "frame_%05d.jpg" % frameIdx)
+
+		if start:
+			countdown -= 1
+			if countdown == 0:
+				buffer.append((start/fps, (end-start)/fps))
+				# out.append((start/fps, (end-start)/fps))
+				# out.append((start, end))
+				# Reset the countdown!
+				countdown = patience
+				start = None
+				end = None
+		if frameIdx % chunk_size == 0:
+			yield buffer
+			buffer = []
+		# print()
+
+	# If there was one flashing light that kept waiting bc of patience
+	if start:
+		yield (start/fps, (end-start)/fps)
+		
 
 # (74, 132, 3)
 # (74, 132, 3)
 # (74, 132)
 
-# Default no kernel (just 1 pixel)
-def convolve_frame(frame, kersize=5):
-	kernel = np.ones((kersize,kersize), dtype=np.float32)
+def convolve_frame(frame: np.ndarray, kersize=5):
 	h,w,c = frame.shape
-	out = np.zeros((h - kersize + 1, w - kersize + 1, c), dtype=np.float32)
-	for i in range(h - kersize + 1):
-		for j in range(w - kersize + 1):
+	# Pad bottom
+	pad_bot = 0
+	if h % kersize != 0:
+		pad_bot = kersize - (h % kersize)
+	# Pad right
+	pad_right = 0
+	if w % kersize != 0:
+		pad_right = kersize - (w % kersize)
+	# To allow for the step size to be the entire kernel
+	frame = np.pad(frame, pad_width=((0,pad_bot), (0,pad_right), (0,0)), mode="constant", constant_values=0)
+	h,w,c = frame.shape
+
+	kernel = np.ones((kersize,kersize), dtype=np.float32)
+	out = np.zeros((h//kersize, w//kersize, c), dtype=np.float32)
+	for i in range(h//kersize):
+		for j in range(w//kersize):
 			for k in range(c):
-				out[i,j,k] = np.sum(frame[i:i+kersize, j:j+kersize, k] * kernel)
+				out[i,j,k] = np.sum(frame[i*kersize:(i+1)*kersize,j*kersize:(j+1)*kersize,k] * kernel)
 	return out.astype(np.int16)
 
-def diff_frames(frame1: np.ndarray, frame2: np.ndarray, threshold=5500):
-	_,_,c = frame1.shape
+def diff_avg(frame1: np.ndarray, frame2: np.ndarray, threshold=1000):
 	# 1. Get diff and take abs
 	# 2. Take the max of all diffs across pixels' kernels in all channels
 	# 3. Summing across all the channels and seeing if this beats threshold
-	diff = np.max(np.abs(frame1[:,:,:]-frame2[:,:,:]))
-	val = np.sum(diff)
-	print("Max difference:", diff)
-	print(f"Found diff:", val)
-	if np.sum(np.max(np.abs(frame1[:,:,:]-frame2[:,:,:]))) >= threshold:
+	diff = np.mean(np.abs(frame1-frame2))
+	# print("RGB Average Diff", diff)
+	if diff >= threshold:
+		return True
+	return False
+
+def diff_bright(frame1: np.ndarray, frame2: np.ndarray, threshold=2500):
+	diff = np.mean(np.abs(frame1[:,:,2]-frame2[:,:,2]))
+	# print("Brightness/Value", diff)
+	if diff >= threshold:
 		return True
 	return False
 
@@ -96,13 +138,16 @@ def ff(idx):
 	frame = cv2.imread("videos/frame_%05d.jpg" % idx)
 	return frame.astype(np.int16)
 
-def test_diff_frames():
+def test_diff_avg():
 	# Literally the same
-	print(diff_frames(ff(2), ff(2)))
+	print(diff_avg(ff(2), ff(2)))
 	# Almost the exact same frame
-	print(diff_frames(ff(2), ff(3)))
+	print(diff_avg(ff(2), ff(3)))
 	# Bomboclaat
-	print(diff_frames(ff(6), ff(7)))
+	print(diff_avg(ff(6), ff(7)))
 
-# test_diff_frames()
-print(compare_frames("videos"))
+# print(compare_frames("videos", 4))
+
+# soba's super stupid test results
+# [(7, 8), (67, 68), (91, 92), (97, 98), (98, 99), (99, 100), (101, 102), (114, 115), (135, 136), (149, 150), (204, 205), (234, 235), (260, 261), (261, 262), (262, 263), (275, 276), (276, 277), (277, 278), (278, 279)]
+# [(7, 7), (67, 150), (204, 279)]
